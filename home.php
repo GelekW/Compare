be4d86c55642b3ba2ecc30063a2ed4a7c159c303
Gelek Wangyal <gelekwangyal@gmail.com>
2018-12-02 21:56:35 -0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <span id="top">
+            <img src=logosmall.png alt="logo" id="logosmall">
+            <span id="account">            
+                <p> Hello, Gelek  <button type="button" class="signout" id="signout" name="submit">Sign Out</button></p>
+                <button type="button" id="signout" name="write">Create New Article</button>
+
+            </span>
+        </span> 
+        <div class="articlebox">
+                <p class="likes">likes</p>
+                <a class="articletitle" href="home.php">I am an article title blah blah</a> <nobr class="text"> by </nobr> <nobr class="author">I am an author</nobr><nobr class="text">, published on </nobr><nobr class="date">00/00/0000</nobr>
+        </div>
+    </div>
+
+</body>
+</html>
